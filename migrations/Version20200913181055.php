75bfa244bf2812edad61674ddb2dd6a6b91533cb
Jacek Wesołowski <jacqu25@yahoo.com>
2020-09-13 20:16:56 +0200

    Registration complete

---
+++ migrations/Version20200913181055.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200913181055 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, email VARCHAR(100) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, deleted_at DATETIME DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->addSql('DROP TABLE user');
+    }
+}
--- src/Entity/Source.php
+++ src/Entity/Source.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=SourceRepository::class)
+ * @ORM\HasLifecycleCallbacks()
  */
 class Source
 {
